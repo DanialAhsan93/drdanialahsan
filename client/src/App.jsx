f8f363472736b0474089e7e06e7f3f6f150d6444
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-05-17 00:43:02 +0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
   return (
     <>
-     <h1 className='text-underline text-red-500 '>Danial Ahsan</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
