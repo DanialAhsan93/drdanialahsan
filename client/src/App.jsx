3ee5b7be4952af65519239892493ecd3f4592ef8
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-05-16 23:51:15 +0500

    intsall react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-underline text-red-500 '>Danial Ahsan</h1>
+    </>
+  )
+}
+
+export default App
