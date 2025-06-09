04889a1cdec57ae4d001c22f0455b36434f3210a
DanialAhsan <danialahsanvirtuenetz@gmail.com>
2025-06-09 21:58:24 +0500

    correction in connecting to the database

--- api/index.js
+++ api/index.js
@@ -4,11 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log('MongoDb is connected')
-}).catch(err => {
- console.log(err)
-})
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected')
+  }).catch(err => {
+    console.log(err)
+  })
 
 const app = express();
 
